3c035f3f97bade14d6312a753d21fc207a36a405
刘珺彦 <2972066691@qq.com>
2022-09-08 10:11:00 +0800

    预览用户头像

--- src/views/user/updateAvatar/index.vue
+++ src/views/user/updateAvatar/index.vue
@@ -5,7 +5,8 @@
   </div>
   <div>
       <!-- 图片，用来展示用户选择的头像 -->
-      <img class="the_img" src="@/assets/images/avatar.jpg" alt="" />
+      <img v-if="!avatar" class="the_img" src="@/assets/images/avatar.jpg" alt="" />
+      <img v-else class="the_img" :src="avatar" alt="" />
 
       <!-- 按钮区域  accept="image/*" 不限制图像的格式-->
       <div class="btn-box">
@@ -28,16 +29,28 @@ export default {
   methods: {
     // 选择图片
     chooseImg () {
+      // 触发点击事件
       this.$refs.iptRef.click()
     },
     // 在选择框中选择图片后触发的改变事件
     onFileChange (e) {
       // 获取用户选择的文件列表（伪数组）
       const files = e.target.files
+      // 没有选择图片
       if (files.length === 0) {
         this.avatar = ''
       } else {
+        // 选择了图片
         console.log(files[0])
+        // 1.创建FileReader对象
+        const fr = new FileReader()
+        // 2.调用readAsDataURL函数，读取文件内容
+        fr.readAsDataURL(files[0])
+        // 3.监听fr的onload事件
+        fr.onload = e => {
+          // 通过e.target.result获取读取的结果，值是字符串base64格式的字符串
+          this.avatar = e.target.result
+        }
       }
     }
   }
